5e72c8c2a9187d32e40240452b2d90d1a42b1700
CodeHz <git@hertz.moe>
2026-04-30 19:46:21 +0800

    移除 EntityBuilder 中已弃用的 withTag 和 withRelationTag 接口

--- src/core/builder.ts
+++ src/core/builder.ts
@@ -27,14 +27,6 @@ export class EntityBuilder {
     return this;
   }
 
-  /**
-   * @deprecated Use `with(componentId)` instead for void components
-   */
-  withTag(componentId: EntityId<void>): this {
-    this.components.push({ type: "component", id: componentId, value: undefined as void });
-    return this;
-  }
-
   withRelation<T>(
     componentId: ComponentId<T>,
     targetEntity: EntityId<any>,
@@ -42,14 +34,6 @@ export class EntityBuilder {
   ): this {
     const value = (args.length > 0 ? args[0] : undefined) as T;
     this.components.push({ type: "relation", componentId, targetId: targetEntity, value });
-    return this;
-  }
-
-  /**
-   * @deprecated Use `withRelation(componentId, targetEntity)` instead for void relations
-   */
-  withRelationTag(componentId: ComponentId<void>, targetEntity: EntityId<any>): this {
-    this.components.push({ type: "relation", componentId, targetId: targetEntity, value: undefined as void });
     return this;
   }
 
